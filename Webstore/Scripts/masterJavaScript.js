5132e7101220ca5d1e827ce077768be64e13a3bb
Alexander Grahn <mralex94@hotmail.com>
2017-10-19 10:04:01 +0200

    Java script fix

--- Webstore/Scripts/masterJavaScript.js
+++ Webstore/Scripts/masterJavaScript.js
@@ -1,8 +1,7 @@
 ﻿$(document).ready(
     function () {
         $(".buttonShow").click(
-            function ()
-            {
+            function () {
                 $(window).attr('location', '/pageProduct2.aspx');
             }
         );
@@ -14,7 +13,18 @@
             }
         );
         $("#goToCheckOut").click(function () {
-            $(window).attr('location', '/pageProduct2.aspx');
+            $(window).attr('location', '/ShoppingCart.aspx');
+        });
+        $("#Pay").click(function () {
+            $(window).attr('location', '/Checkout.aspx');
+        });
+
+        $("#Buy").click(function () {
+            $(window).attr('location', '/Ïndex.aspx');
+            alert("Tack för din beställning");
+        });
+        $("#Cancel").click(function () {
+            $(window).attr('location', '/ShoppingCart.aspx');
         });
     }
 ); 
